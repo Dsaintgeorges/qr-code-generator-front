ce9447731c144a6667834236079d54ff59796e6e
Doryan <doryan@MacBook-Air-de-Doryan.local>
2022-12-16 21:52:23 +0100

    adding const to set API + docker and style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import {useState} from "react";
+import {API_URL} from "./shared/const";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [name, setName] = useState("")
+    const [url, setUrl] = useState("")
+    const [image, setImage] = useState("")
+    const obj = {
+        container:'form-container'
+    }
+    const getData = async () => {
+
+       await axios.post(API_URL+"generate_classic_qr_code", {
+            name,
+           url
+        },{maxContentLength:Infinity,maxBodyLength:Infinity,responseType:"blob",} ).then(
+            (response) => {
+                let image = URL.createObjectURL(response.data);
+               setImage(image)
+            }
+        )
+
+    }
+
+    return (
+        <div className="App">
+            <div className={obj.container}>
+                <label className="label">
+                    Name:
+                    <input type={"text"} onChange={(e)=> {
+                        setName(e.target.value)
+                    }}/>
+                </label>
+                <label className="label">
+                    Url:
+                    <input type={"text"}
+                           onChange={(e)=> {setUrl(e.target.value)}}
+                           onKeyPress={(e)=>e.key==='Enter'?getData():null}
+                    />
+                </label>
+
+                <button className="validate-button" onClick={getData}>Créer son QR-CODE</button>
+            </div>
+           <div className="image">
+               <img src={image} alt={"qr-code"}/>
+           </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/shared/const.js
@@ -0,0 +1,1 @@
+export const API_URL = "http://qr-code.dlsg.be:8080/";
